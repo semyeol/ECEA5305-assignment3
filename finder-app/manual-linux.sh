efcfa18a42599bb4305e12c794565453c8243fe5
vboxuser223 <vboxuser223@Ubuntu.myguest.virtualbox.org>
2026-04-02 22:22:10 -0700

    copy kernel build to OUTDIR/image

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -46,6 +46,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 fi 
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
