9b25eda206dedda46f32291e2f47e9b836bd35a3
vboxuser223 <vboxuser223@Ubuntu.myguest.virtualbox.org>
2026-04-04 19:43:11 -0700

    moved shebang to line 1 of writer.sh and finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Accepts the following runtime arguments: the first argument is a path to a directory on the filesystem, referred to below as filesdir; 
 # the second argument is a text string which will be searched within these files, referred to below as searchstr
 
@@ -8,8 +10,6 @@
 # Prints a message "The number of files are X and the number of matching lines are Y" where X is the number of files in the directory and 
 # all subdirectories and Y is the number of matching lines found in respective files, where a matching line refers to a line which contains 
 # searchstr (and may also contain additional content).
-
-#!/bin/sh
 
 filesdir=$1
 searchstr=$2
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Accepts the following arguments: the first argument is a full path to a file (including filename) on the filesystem, referred to below as writefile; 
 # the second argument is a text string which will be written within this file, referred to below as writestr
 
@@ -5,8 +7,6 @@
 
 # Creates a new file with name and path writefile with content writestr, overwriting any existing file and creating the path if it doesn’t exist. 
 # Exits with value 1 and error print statement if the file could not be created.
-
-#!/bin/sh
 
 writefile=$1
 writestr=$2
